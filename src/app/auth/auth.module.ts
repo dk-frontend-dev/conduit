9500184f1a2fa8ccfafb6a87b4d6c5b0bdf55b03
zhenia kviatkivskiy <zhenia.kviat@gmail.com>
2022-06-02 15:36:09 +0300

    feat: create register component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,12 @@ import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
 
 import {LoginComponent} from '@/auth/components/login/login.component'
-import { AuthRoutingModule } from '@/auth/auth-routing.module'
+import { AuthRoutingModule } from '@/auth/auth-routing.module';
+import { RegisterComponent } from './components/register/register.component'
 
 @NgModule({
   imports: [CommonModule, AuthRoutingModule],
-  declarations: [LoginComponent],
-  exports: [LoginComponent]
+  declarations: [LoginComponent, RegisterComponent],
+  exports: [LoginComponent,RegisterComponent]
 })
 export class AuthModule {}
